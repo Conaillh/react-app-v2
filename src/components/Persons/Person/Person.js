89dbc4a5c49d3712e196453e47ae7ef9d5c2a35e
Conaill Hyndman <chyndman@deloitte.co.uk>
2018-12-06 12:07:58 +0000

    creating login function with authentication

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classes from './Person.css';
 import withClass from '../../../hoc/WithClass';
 import Aux from '../../../hoc/Aux';
+import { AuthContext } from '../../../containers/App'
 
 class Person extends Component {
     constructor(props) {
@@ -17,7 +18,7 @@ class Person extends Component {
 
     componentDidMount() {
         console.log('[Person.js] Inside componentDidMount()');
-        if (this.props.position ===0) {
+        if (this.props.position === 0) {
             this.inputElement.current.focus();
         }
     }
@@ -30,7 +31,12 @@ class Person extends Component {
         console.log('[Person.js] Inside render()');
         return (
             <Aux>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+                <AuthContext.Consumer>
+                    {auth => auth ? <p>I'm authenticated!</p> : null}
+                </AuthContext.Consumer>
+                <p onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old
+                </p>
                 <p>{this.props.children}</p>
                 <input
                     ref={this.inputElement}
